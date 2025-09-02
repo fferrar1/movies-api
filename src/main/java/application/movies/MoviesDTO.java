241c634dccba92383a5d60959aa1405043fb2e9f
Fernanda Ferreira Ferrari <110040514+fferrar1@users.noreply.github.com>
2025-09-02 18:36:09 +0000

    [NEW] Criando DTO do filme

---
+++ src/main/java/application/movies/MoviesDTO.java
@@ -0,0 +1,12 @@
+package application.movies;
+
+import application.genres.GenresDTO;
+
+public record MoviesDTO(long id, String title, GenresDTO genre) {
+    MoviesDTO (Movies data) {
+        this(
+            data.getId(), 
+            data.getTitle(), 
+            new GenresDTO(data.getGenre()));
+    }
+}
