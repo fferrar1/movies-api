be1a3cf26ec000dbfe2b68841969c110fd9cd4d6
Fernanda Ferreira Ferrari <110040514+fferrar1@users.noreply.github.com>
2025-09-09 17:26:41 +0000

    [NEW] Transformando o MoviesDTO em público

--- src/main/java/application/movies/MoviesDTO.java
+++ src/main/java/application/movies/MoviesDTO.java
@@ -3,7 +3,7 @@ package application.movies;
 import application.genres.GenresDTO;
 
 public record MoviesDTO(long id, String title, GenresDTO genre) {
-    MoviesDTO (Movies data) {
+    public MoviesDTO (Movies data) {
         this(
             data.getId(), 
             data.getTitle(), 
